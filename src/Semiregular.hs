81674c80064900969fba2ed924ffc0584e9b80ec
Robin Gieseking <robinagieseking@gmail.com>
2026-04-12 16:23:01 -0400

    Add definitions for truncated and kis semiregular polyhedra

--- src/Semiregular.hs
+++ src/Semiregular.hs
@@ -7,12 +7,23 @@ module Semiregular
     deltoidalIcositetrahedron,
     rhombicosidodecahedron,
     deltoidalHexecontahedron,
+    truncatedTetrahedron,
+    triakisTetrahedron,
+    truncatedCube,
+    triakisOctahedron,
+    truncatedOctahedron,
+    tetrakisHexahedron,
+    truncatedDodecahedron,
+    triakisIcosahedron,
+    truncatedIcosahedron,
+    pentakisDodecahedron,
   )
 where
 
 import Operators
 import Platonic
 import Polyhedron
+import Space
 
 cuboctahedron :: Polyhedron (Edge Octant SignedAxis) (Either Octant SignedAxis)
 cuboctahedron = amboPolyhedron cube
@@ -37,3 +48,33 @@ rhombicosidodecahedron = expandedPolyhedron dodecahedron
 
 deltoidalHexecontahedron :: Polyhedron (ExpandedFace DodecahedronVertex IcosahedronVertex) (DodecahedronVertex, IcosahedronVertex)
 deltoidalHexecontahedron = dualPolyhedron rhombicosidodecahedron
+
+truncatedTetrahedron :: Polyhedron ((Sign, Sign, Sign), (Sign, Sign, Sign)) (Either (Sign, Sign, Sign) (Sign, Sign, Sign))
+truncatedTetrahedron = truncatedPolyhedron tetrahedron
+
+triakisTetrahedron :: Polyhedron (Either (Sign, Sign, Sign) (Sign, Sign, Sign)) ((Sign, Sign, Sign), (Sign, Sign, Sign))
+triakisTetrahedron = dualPolyhedron truncatedTetrahedron
+
+truncatedCube :: Polyhedron (Octant, Octant) (Either Octant SignedAxis)
+truncatedCube = truncatedPolyhedron cube
+
+triakisOctahedron :: Polyhedron (Either Octant SignedAxis) (Octant, Octant)
+triakisOctahedron = dualPolyhedron truncatedCube
+
+truncatedOctahedron :: Polyhedron (SignedAxis, SignedAxis) (Either SignedAxis Octant)
+truncatedOctahedron = truncatedPolyhedron octahedron
+
+tetrakisHexahedron :: Polyhedron (Either SignedAxis Octant) (SignedAxis, SignedAxis)
+tetrakisHexahedron = dualPolyhedron truncatedOctahedron
+
+truncatedDodecahedron :: Polyhedron (DodecahedronVertex, DodecahedronVertex) (Either DodecahedronVertex IcosahedronVertex)
+truncatedDodecahedron = truncatedPolyhedron dodecahedron
+
+triakisIcosahedron :: Polyhedron (Either DodecahedronVertex IcosahedronVertex) (DodecahedronVertex, DodecahedronVertex)
+triakisIcosahedron = dualPolyhedron truncatedDodecahedron
+
+truncatedIcosahedron :: Polyhedron (IcosahedronVertex, IcosahedronVertex) (Either IcosahedronVertex DodecahedronVertex)
+truncatedIcosahedron = truncatedPolyhedron icosahedron
+
+pentakisDodecahedron :: Polyhedron (Either IcosahedronVertex DodecahedronVertex) (IcosahedronVertex, IcosahedronVertex)
+pentakisDodecahedron = dualPolyhedron truncatedIcosahedron
